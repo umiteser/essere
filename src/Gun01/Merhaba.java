d986a40c48ce2df4b6a1fbb50dccf2d4e37183ca
umiteser <eser_umit@hotmail.com>
2024-02-11 18:37:53 +0300

    Sepet ürün eklenmesi ve kontolü testi eklendi.

---
+++ src/Gun01/Merhaba.java
@@ -0,0 +1,24 @@
+package Gun01;
+
+public class Merhaba {
+
+    public static void main(String[] args) {
+
+        System.out.println("Merhaba");
+        System.out.println("Merhaba2");
+
+    }
+
+
+    /*
+    GİT KAVRAMLARI
+    dosya rengi kırmızı ise: git tarafından takip dışı
+    dosya rengi yeşil ise: git takibinde
+
+    takip edilmeye: Trancking (dosya takip ediliyor)
+    commit: değişiklikleri yaptım, açıklaması yani etiketi verilerek etiket edilmesi.
+    genelden jira dan gelen taskın adı yazılır.
+    Commit'e yaptığın işi yazıyorsun.
+
+     */
+}
